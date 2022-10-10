e64de292442c41ce62de4274755d67303876817a
chowdhuryannoor <chowdhuryannoor@gmail.com>
2022-10-09 22:45:08 -0400

    Basic login to google works. Callback is still not defined.

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,24 @@
+const google = require('googleapis').google 
+const CONFIG = require('../configs.js')
+
+login = (req, res) => {
+    // Create an OAuth2 client object
+    const OAuth2 = google.auth.OAuth2
+    const auth_client = new OAuth2(
+        CONFIG.oauth_credentials.client_id, 
+        CONFIG.oauth_credentials.client_secret, 
+        CONFIG.oauth_credentials.redirect_uris[0]
+    );
+    
+    // Obtain the google login link 
+    const auth_url = auth_client.generateAuthUrl({
+        access_type: 'offline', //Does not require user to constantly give consent
+        scope: CONFIG.oauth_credentials.scopes 
+    });
+
+    return res.redirect(auth_url);
+}
+
+module.exports = {
+    login: login
+}

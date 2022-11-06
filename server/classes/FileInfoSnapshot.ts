51fb18ca98a4e34c971a8e5c9eece05ad381eab8
Prakash Sekar <pkhsekar@gmail.com>
2022-11-06 01:44:57 -0400

    added method to generate FileInfoSnapshots from snapshotModels

--- server/classes/FileInfoSnapshot.ts
+++ server/classes/FileInfoSnapshot.ts
@@ -4,6 +4,10 @@ import { Query } from "./Query"
 import { operatorToQueryPredicate, QueryPredicate } from "../predicates"
 import Models from "../db/Models"
 import { Types } from "mongoose"
+import { fileSnapshotModel } from "../db/Models/FileSnapshotSchema"
+import { DriveFolder } from "./DriveFolder"
+import { Permission } from "./Permission"
+import { User } from "./User"
 
 export class FileInfoSnapshot {
     constructor (
@@ -12,6 +16,32 @@ export class FileInfoSnapshot {
         public drive_roots: DriveRoot[], 
         public date_updated: Date
     ) {}
+
+    static async retrieve(id: Types.ObjectId): Promise<FileInfoSnapshot> {
+        let snapshot: any = await fileSnapshotModel.findById(id)
+        let roots: DriveRoot[] = []
+        let idToDriveFile: Map<string, [DriveFile, Types.ObjectId[]]> = new Map<string, [DriveFile, Types.ObjectId[]]>()
+        snapshot.files.forEach((file: any) => {
+            let permissions: Permission[] = file.permissions.map((p: any) => new Permission(p._id, p.drive_id, new User("none",  "none"), p.role))
+            let drivefile: DriveFile
+            switch (file.type) {
+                case "ROOT": drivefile = new DriveRoot(file._id, file.drive_id, "", [], false); roots.push(drivefile as DriveRoot); break
+                case "FILE": drivefile = new DriveFile(file._id, file.drive_id, null, new Date(), new Date(), file.name, new User("none",  "none"), permissions, new User("none",  "none"), file.mime_type); break
+                case "FOLDER": drivefile = new DriveFolder(file._id, file.drive_id, null, new Date(), new Date(), file.name, new User("none",  "none"), permissions, new User("none",  "none"), file.mime_type, []); break
+                default: throw new Error("file does not have a valid type")
+            }
+            idToDriveFile.set(file._id.toString(), [drivefile, file.children])
+        })
+        idToDriveFile.forEach(([file, childrenIDs]: [DriveFile, Types.ObjectId[]], key: string) => {
+            if (childrenIDs.length === 0) return
+            childrenIDs.forEach((childID: Types.ObjectId) => {
+                let child: DriveFile = (idToDriveFile.get(childID.toString()) as [DriveFile, Types.ObjectId[]])[0];
+                (file as DriveFolder).children.push(child);
+                child.parent = (file as DriveFolder)
+            })
+        })
+        return new FileInfoSnapshot(snapshot._id, snapshot.createdAt, roots, snapshot.updatedAt)
+    }
 
     applyQuery(query: Query): DriveFile[] {
         let f: QueryPredicate = operatorToQueryPredicate[query.operator]
@@ -27,21 +57,16 @@ export class FileInfoSnapshot {
         return copy
     }
 
-    save(callback: (err: Error) => void): void {
+    async save(callback: (err: Error) => void): Promise<Types.ObjectId> {
         const files = this.drive_roots.flatMap((d: DriveRoot) => d.getModel())
-        const snapshotModel = new Models.FileSnapshotModel(
-            {
-                user_id: this._id,
-                files: files
-            }
-        )
-
-        snapshotModel.save((err, result) => {
-            if(err){
-                console.log("Error saving file snapshot: ", err)
-            }
-        })
-
+        const snapshotModel = new Models.FileSnapshotModel({ user_id: this._id, files: files })
+        let res: any
+        try {
+            res = await snapshotModel.save()
+        } catch (e) {
+            console.log("Error saving file snapshot: ", e)
+        }
+        return res._id
     }
 
     toString(): string {

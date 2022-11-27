20dbff1079a34bc38ab125207b5d0f30a2758eee
chowdhuryannoor <chowdhuryannoor@gmail.com>
2022-11-26 19:32:28 -0500

    Added access control policies

--- server/controllers/userProfileController.ts
+++ server/controllers/userProfileController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express"
 import { getModel } from "../middleware/getUserModel"
+import { Types } from 'mongoose'
+import Models from '../db/Models';
 
 
 const getSavedQueries = async (req: Request, res: Response) => {
@@ -15,14 +17,71 @@ const saveQuery = async (req: Request, res: Response) => {
 }
 
 const getAccessControlPolicies = async (req: Request, res: Response) => {
+    //get the userModel
+    let user: any = await getModel(req.cookies.jwt)
+
     res.status(200).json({ 
-        status: 'OK' 
+        status: 'OK',
+        Policies: JSON.stringify(user.AccessControlPolicy)
     })
 }
 
 const addAccessControlPolicy = async (req: Request, res: Response) => {
+    //get the user model
+    let user: any = await getModel(req.cookies.jwt)
+
+    const payload = req.body
+
+    //create driveUsers
+    let allowedReaders = payload.allowedReaders.map((driveUser: any) => {
+        return new Models.DriveUserModel({
+            type: driveUser.type,
+            email: driveUser.email,
+            displayName: driveUser.displayName
+        })
+    })
+
+    let deniedReaders = payload.deniedReaders.map((driveUser: any) => {
+        return new Models.DriveUserModel({
+            type: driveUser.type,
+            email: driveUser.email,
+            displayName: driveUser.displayName
+        })
+    })
+
+    let allowedWriters = payload.allowedWriters.map((driveUser: any) => {
+        return new Models.DriveUserModel({
+            type: driveUser.type,
+            email: driveUser.email,
+            displayName: driveUser.displayName
+        })
+    })
+
+    let deniedWriters = payload.deniedWriters.map((driveUser: any) => {
+        return new Models.DriveUserModel({
+            type: driveUser.type,
+            email: driveUser.email,
+            displayName: driveUser.displayName
+        })
+    })
+
+    //create the policy
+    let newPolicy = new Models.ACPModel({
+        allowedReaders: allowedReaders,
+        deniedReaders: deniedReaders,
+        allowedWriters: allowedWriters,
+        deniedWriters: deniedWriters,
+        isGroup: false
+    })
+
+    //add the policy to the user model
+    user.AccessControlPolicy.push(newPolicy)
+    await user.save()
+    console.log(JSON.stringify(newPolicy))
+
     res.status(200).json({ 
-        status: 'OK' 
+        status: 'OK',
+        AccessControlPolicy: JSON.stringify(newPolicy)
     })
 }
 

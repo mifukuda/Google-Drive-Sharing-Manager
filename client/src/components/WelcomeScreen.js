123525b3a423f7fdfd916a3cded927a0cb754dbd
Minato Fukuda <minato.fukuda@stonybrook.edu>
2022-11-30 16:57:21 -0500

    Log in with OneDrive

--- client/src/components/WelcomeScreen.js
+++ client/src/components/WelcomeScreen.js
@@ -4,11 +4,16 @@ import React from "react";
 export default function WelcomeScreen() {
     return (
         <div className="welcomescreen">
-            <p>Welcome to The Microservice's</p>
-            <p>Drive Sharing Manager</p>
-            <a className="btn btn-outline-dark" role="button" style={{margin: 'auto', height: '45px'}} href="http://localhost:4000/auth/login">
+            <h1 style={{marginBottom: '1%'}}>Welcome to The Microservice's</h1>
+            <h1 style={{marginBottom: '2%'}}>Drive Sharing Manager</h1>
+            <a className="btn btn-outline-dark" role="button" style={{margin: 'auto', height: '45px', width: '250px', marginBottom:'1%'}} href="http://localhost:4000/auth/login">
                 <img src={require('../images/googlelogo.png')} style={{height: '70%', marginRight: '10px', }} alt="Google Logo"/>
                 Log In With Google
+            </a>
+            <p style={{margin: '0'}}>-or-</p>
+            <a className="btn btn-outline-dark" role="button" style={{margin: 'auto', height: '45px', width: '250px', marginTop:'1%'}} href="http://localhost:4000/auth/mslogin">
+                <img src={require('../images/onedrivelogo.png')} style={{height: '70%', marginRight: '10px', }} alt="Google Logo"/>
+                Log In With OneDrive
             </a>
         </div>
     );

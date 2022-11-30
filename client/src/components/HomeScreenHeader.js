845035efa526eaec08ab0136ea5f91f7fcbb9646
Minato Fukuda <minato.fukuda@stonybrook.edu>
2022-11-30 15:40:29 -0500

    Logout redirects to login screen

--- client/src/components/HomeScreenHeader.js
+++ client/src/components/HomeScreenHeader.js
@@ -1,13 +1,16 @@
 import React from "react";
 import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom'
 import {Popover, OverlayTrigger} from 'react-bootstrap';
 
 // Button redirects to Google Auth
 export default function HomeScreenHeader() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     function handleLogOut() {
         dispatch({type:'LOG_OUT'});
+        navigate("/");
     }
 
     const popover = (
